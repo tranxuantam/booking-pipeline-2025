650403fa3646a0b719e928c4ee337f4694782617
tranxuantam <tranxuantam2004@gmail.com>
2025-11-28 01:11:37 +0700

    4_fact_hotel_prices.sql

--- sql/4_fact_hotel_prices.sql
+++ sql/4_fact_hotel_prices.sql
@@ -1,5 +1,6 @@
---fact_hotel_prices
-CREATE OR REPLACE TABLE booking_dataset.fact_hotel_prices AS
+-- 1. Tạo bảng tạm cho fact_hotel_prices
+CREATE OR REPLACE TABLE `booking-project-479502.booking_dataset.fact_hotel_prices_temp` AS
+
 SELECT
   h.hotel_key,
   din.date_key   AS checkin_date_key,
@@ -15,7 +16,31 @@ SELECT
   dout.full_date      AS checkout_date,
   dout.is_peak_holiday AS checkout_is_peak_holiday,
   f.crawl_time
-FROM booking_dataset.stg_booking_hotels f
-JOIN booking_dataset.dim_hotels h USING (city, hotel_name)
-JOIN booking_dataset.dim_date din  ON f.checkin_date = din.date_key
-JOIN booking_dataset.dim_date dout ON f.checkout_date = dout.date_key;
+FROM `booking-project-479502.booking_dataset.stg_booking_hotels` f
+JOIN `booking-project-479502.booking_dataset.dim_hotels` h USING (city, hotel_name)
+JOIN `booking-project-479502.booking_dataset.dim_date` din  ON f.checkin_date = din.date_key
+JOIN `booking-project-479502.booking_dataset.dim_date` dout ON f.checkout_date = dout.date_key;
+
+-- 2. Kiểm tra và gộp dữ liệu cho fact_hotel_prices
+BEGIN
+  DECLARE fact_hotel_prices_exists BOOL DEFAULT (
+    SELECT COUNT(*) > 0 
+    FROM `booking-project-479502.booking_dataset.INFORMATION_SCHEMA.TABLES` 
+    WHERE table_name = 'fact_hotel_prices'
+  );
+  
+  -- Nếu bảng chính chưa tồn tại, tạo bảng mới
+  IF NOT fact_hotel_prices_exists THEN
+    CREATE OR REPLACE TABLE `booking-project-479502.booking_dataset.fact_hotel_prices` AS
+    SELECT * FROM `booking-project-479502.booking_dataset.fact_hotel_prices_temp`;
+  ELSE
+    -- Nếu bảng chính đã tồn tại, gộp dữ liệu cũ + mới
+    CREATE OR REPLACE TABLE `booking-project-479502.booking_dataset.fact_hotel_prices` AS
+    SELECT * FROM `booking-project-479502.booking_dataset.fact_hotel_prices`  -- DỮ LIỆU CŨ
+    UNION ALL
+    SELECT * FROM `booking-project-479502.booking_dataset.fact_hotel_prices_temp`;  -- DỮ LIỆU MỚI
+  END IF;
+END;
+
+-- 3. Xóa bảng tạm
+DROP TABLE IF EXISTS `booking-project-479502.booking_dataset.fact_hotel_prices_temp`;
